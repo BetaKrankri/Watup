c1282c2f58acf0fa253837fd3ecab32f69d743ed
BetaKrankri <edominguezj11@outlook.com>
2023-05-15 01:36:22 -0600

    component: Add profileMenu & components

--- src/components/ChatNav.jsx
+++ src/components/ChatNav.jsx
@@ -1,10 +1,17 @@
+import ProfileMenu from "./ProfileMenu";
+import Avatar from "./Avatar.jsx";
+import Submenu from './Submenu.jsx';
+
 const ChatNav = () => {
   return (
-    <div className="w-full h-full flex flex-col bg-black">
-        ProfileSection
-        ChatsSection
+    <div className="w-full h-full flex flex-col  bg-black">
+      <ProfileMenu>
+        <Avatar width={50}/>
+        <Submenu />
+      </ProfileMenu>
+      ChatsSection
     </div>
-  )
-}
+  );
+};
 
-export default ChatNav
+export default ChatNav;
---
+++ src/components/Submenu.jsx
@@ -0,0 +1,11 @@
+import { VerticalDotsIcon } from "../assets/icons";
+
+const Submenu = () => {
+  return (
+    <div className="submenu-icon w-7 h-7 grid place-items-center overflow-hidden">
+      <VerticalDotsIcon className="w-full h-full fill-slate-200" />
+    </div>
+  );
+};
+
+export default Submenu;
