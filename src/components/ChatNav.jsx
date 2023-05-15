b883b2bb3c152c45a48499657831ad8c328e1f28
BetaKrankri <edominguezj11@outlook.com>
2023-05-14 22:15:38 -0600

    component: ChatNav

---
+++ src/components/ChatNav.jsx
@@ -0,0 +1,10 @@
+const ChatNav = () => {
+  return (
+    <div className="w-full h-full flex flex-col bg-black">
+        ProfileSection
+        ChatsSection
+    </div>
+  )
+}
+
+export default ChatNav
