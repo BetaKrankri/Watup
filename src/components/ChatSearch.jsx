4d52fbc148af09bd6ade027dc5f55c7eb2dffc40
BetaKrankri <edominguezj11@outlook.com>
2023-05-15 03:49:46 -0600

    chore: attahc state to SearchBox

--- src/components/ChatSearch.jsx
+++ src/components/ChatSearch.jsx
@@ -1,13 +1,15 @@
 import { SearchIcon } from "../assets/icons";
 
-const ChatSearch = () => {
+const ChatSearch = ({value, onChange}) => {
   return (
     <div className="SearchInput w-full min-h-[40px] flex items-center px-3 gap-3 overflow-hidden rounded-2xl bg-slate-300 focus-within:ring-2 focus-within:ring-slate-500 focus-within:border-slate-500">
       <SearchIcon className="search-icon w-7 h-7" />
       <input
         type="text"
         placeholder="Buqueda de Chat"
-        className="flex-1 bg-transparent appearance-none border-none outline-none"
+        className="flex-1 bg-transparent appearance-none border-none outline-none text-slate-950"
+        value={value}
+        onChange={onChange}
       />
     </div>
   );
--- src/components/ChatsSection.jsx
+++ src/components/ChatsSection.jsx
@@ -1,12 +1,17 @@
 import ChatSearch from "./ChatSearch";
 import ChatItem from "./ChatItem.jsx";
+import { useState } from "react";
 
 const ChatsSection = ({ children }) => {
+  const [searchTerm, setSearchTerm] = useState("");
   return (
     <div className="w-full h-full flex flex-col bg-slate-950">
       {/* Top Search Bar */}
       <div className="w-full p-3">
-        <ChatSearch value={""} onClick={() => {}} />
+        <ChatSearch
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+        />
       </div>
       {/* Bottom Chat List  */}
       <div className="ChatList h-full w-full bg-slate-950">
