629d8918da878a775b3a7723a72027bc273baabc
BetaKrankri <edominguezj11@outlook.com>
2023-05-15 22:07:09 -0600

    chore: add Data js's

---
+++ src/store/data/contacts.js
@@ -0,0 +1,7 @@
+const contacts = [
+    { name: "Loui de Pastrua", phone: "+56876745" },
+    { name: "Bruno Salero", phone: "+5637845678" },
+    { name: "Lidia Pino", phone: "+7894567839" },
+  ];
+
+export default contacts
---
+++ src/store/data/conversations.js
@@ -0,0 +1,56 @@
+let conversations = [
+  {
+    id: 1,
+    chatName: "Bruno Salero",
+    messages: [
+      {
+        timeStamp: "12:00",
+        text: "Hola",
+        sender: "Bruno Saleiro",
+      },
+      {
+        timeStamp: "12:03",
+        text: "HEY!! La tarea!!!",
+        sender: "ME",
+      },
+      {
+        timeStamp: "12:05",
+        text: "Que tarea?",
+        sender: "Bruno Saleiro",
+      },
+      {
+        timeStamp: "12:06",
+        text: "Aaah No hay Je :)",
+        sender: "ME",
+      },
+    ],
+  },
+  {
+    id: 2,
+    chatName: "Lidia Pino",
+    messages: [
+      {
+        timeStamp: "14:17",
+        text: "Si tal vez",
+        sender: "Lidia Pino",
+      },
+      {
+        timeStamp: "15:03",
+        text: "Pero como no",
+        sender: "ME",
+      },
+      {
+        timeStamp: "15:05",
+        text: "Lorem Ipsum Asumachina",
+        sender: "Lidia Pino",
+      },
+      {
+        timeStamp: "16:06",
+        text: "Ilieia",
+        sender: "Lidia Pino",
+      },
+    ],
+  },
+];
+
+export default conversations;
